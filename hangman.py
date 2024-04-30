7903d84244d026f5527debfa664670981994bd1e
Hairum Qureshi <hqureshi@udel.edu>
2024-04-29 21:57:54 -0400

    added code to read from the JSON files and display a welcome message

--- hangman.py
+++ hangman.py
@@ -4,3 +4,16 @@
 # the user can use points to buy hints
 # there are 3 game modes: easy, medium, and hard
 
+import json
+
+with open("./words/easy.json") as json_file:
+    easy_words = json.load(json_file)
+
+with open("./words/medium.json") as json_file:
+    medium_words = json.load(json_file)
+
+with open("./words/hard.json") as json_file:
+    hard_words = json.load(json_file)
+
+greeting_message = "Hello, welcome to hangman! What game level will you like to play? \n > Easy \n > Medium \n > Hard"
+print(greeting_message)
